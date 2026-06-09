ce32bc14fcab63bc8599d4fb7900167e505ff253
anassmester004 <anassmester004@gmail.com>
2026-06-09 15:30:25 +0200

    Etape 1

--- 01-basics/loops.py
+++ 01-basics/loops.py
@@ -1,2 +1,5 @@
-for i in range(5):
-    print(i)        # affiche 0, 1, 2, 3, 4
+n = int(input("Donne un nombre : "))
+somme = 0
+for i in range(1, n + 1):
+ somme = somme + i
+print("La somme est :", somme)
