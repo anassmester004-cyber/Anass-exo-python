2a7daab3f33ab16f8f2f04f9bc8f23bca869bf77
anassmester004 <anassmester004@gmail.com>
2026-06-10 00:31:07 +0200

    Etape 1

---
+++ 02-data-structures/dictionaries.py
@@ -0,0 +1,12 @@
+annuaire = {
+    "Lucas": "0601020304",
+    "Anass": "0611223344",
+    "Sarah": "0655667788"
+}
+
+nom = input("Quel nom cherches-tu ? ")
+
+if nom in annuaire:
+    print("Numéro :", annuaire[nom])
+else:
+    print("Contact introuvable")
